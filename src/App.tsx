f0311d412d01162e9d8da1368910c51cdbbb8e77
huuhung0968374305 <huuhung0968374305@gmail.com>
2024-04-07 21:50:40 +0700

    register

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,35 @@ import PrivateRoute from './components/PrivateRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Protected from './pages/Protected'
+import Signup from './pages/Signup'
+import { ConfigProvider } from 'antd'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route
-          path='/protected'
-          element={
-            <PrivateRoute>
-              <Protected /> {/*  Protected component */}
-            </PrivateRoute>
-          }
-        />
-        <Route path='*' element={<Home />} /> {/* Default route */}
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: 'Arial, sans-serif;',
+          colorText: 'rgba(0, 0, 0, 0.9)'
+        }
+      }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Signup />} />
+          <Route
+            path='/protected'
+            element={
+              <PrivateRoute>
+                <Protected /> {/*  Protected component */}
+              </PrivateRoute>
+            }
+          />
+          <Route path='*' element={<Home />} /> {/* Default route */}
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   )
 }
 
